3f56e3629c25def1f001772ab9c18371b8b3ef26
Ryan <ryanrichter028@gmail.com>
2023-01-28 04:02:56 -0500

    temporary expansion code

---
+++ include/subsys/expansion.hpp
@@ -0,0 +1,28 @@
+#include "main.h"
+
+#ifndef _EXPANSION_H_
+#define _EXPANSION_H_
+
+namespace p
+{
+    inline const int expansion = 20;
+}
+
+namespace m
+{
+    extern pros::Motor expansion;
+}
+
+namespace ctrl
+{
+    extern pros::controller_digital_e_t exp1;
+    extern pros::controller_digital_e_t exp2;
+    extern pros::controller_digital_e_t exp3;
+}
+
+namespace expansion
+{
+    void opcon();
+}
+
+#endif
---
+++ src/subsys/expansion.cpp
@@ -0,0 +1,26 @@
+#include "subsys/expansion.hpp"
+#include "subsys/globals.hpp"
+
+// define motors
+pros::Motor m::expansion (p::expansion, pros::E_MOTOR_GEARSET_18, false, pros::E_MOTOR_ENCODER_DEGREES);
+
+// define controls
+pros::controller_digital_e_t ctrl::exp1 = pros::E_CONTROLLER_DIGITAL_A;
+pros::controller_digital_e_t ctrl::exp2 = pros::E_CONTROLLER_DIGITAL_LEFT;
+pros::controller_digital_e_t ctrl::exp3 = pros::E_CONTROLLER_DIGITAL_R2;
+
+namespace expansion
+{
+    void shoot()
+    {
+        m::expansion.move_relative(360, 200);
+    }
+
+    void opcon()
+    {
+        if (ctrl::master.get_digital(ctrl::exp1)
+            && ctrl::master.get_digital(ctrl::exp2)
+            && ctrl::master.get_digital_new_press(ctrl::exp3))
+            shoot();
+    }
+}
