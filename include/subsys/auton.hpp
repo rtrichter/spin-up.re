32703d93225c2ace9fd08ab06dcdef353a308166
Ryan Richter <ryanrichter028@gmail.com>
2023-02-25 23:46:52 -0500

    pid prototyped

--- include/subsys/auton.hpp
+++ include/subsys/auton.hpp
@@ -3,6 +3,85 @@
 #ifndef _AUTON_H_
 #define _AUTON_H_
 
+/*
 
+https://en.wikipedia.org/wiki/PID_controller
+
+P: Proportional
+I: Integral
+D: Derivative 
+(think 0th, -1st, and 1st derivatives)
+
+P:
+SP = setpoint (target value)
+PV = process variable (measured value)
+P = SP-PV (difference between actual and expected)
+
+Integral:
+Accounts for the past values of SP-PV error
+
+Derivative:
+Predictive control
+
+OUR APPLICATION:
+
+Proportional:
+PV = rotation sensor value (converted)
+SP = target value
+e = SP-PV
+
+Integral:
+makes the input increase over time until the target is reached
+
+Derivative:
+Decreases control at end of move
+
+
+Kp
+Ki
+Kd
+
+error
+prevError
+derivative
+integral 
+
+
+PV = avgEncoderValue
+error = SP-PV
+
+derivative = error - prevError
+
+integral += error (exclude from drive?)
+
+voltage = Kp*error + Ki*integral + Kd*derivative
+
+
+
+
+
+prevError = error
+pros::delay(20)
+
+
+
+*/
+
+namespace flywheel
+{
+    void PID(int velocity);
+}
+
+namespace drive
+{
+    inline const double Kpt = 0.0;
+    inline const double Kit = 0.0;
+    inline const double Kdt = 0.0;
+    inline const double Kpr = 0.0;
+    inline const double Kir = 0.0;
+    inline const double Kdr = 0.0;
+    void translate(int distance);
+    void rotate(int degrees);
+}
 
 #endif
--- src/subsys/auton.cpp
+++ src/subsys/auton.cpp
@@ -9,3 +9,55 @@
 #include "sens.hpp"
 
 #include <cmath>
+
+namespace flywheel
+{
+
+}
+
+namespace drive
+{
+    int enc2dist(int encoder_value)
+    {
+        /*
+        enc_value
+        (4in*PI/enc_value degrees) inches
+        (100 centitiles/24 inches) 
+        */
+    }
+
+    /*
+
+    AUTOTUNING
+
+    try translate at varying Kp values
+    
+
+    
+    */
+
+    void translate(int distance)
+    {
+        // convert distance to encoder units
+        sens::tare_drive_encoders();
+        int current;
+        int voltage;
+        int error;
+        int prev_error = 0;
+        int derivative;
+        int integral;
+        // while not at target location and moving
+        while ( error && sens::avg_drive_encoder_velocity() ) 
+        {
+            current = sens::avg_drive_encoder();
+            current = (sens::left.get_position() + sens::right.get_position()) / 2;
+            error = distance - current;
+            integral += error;
+            derivative = error - prev_error;
+            voltage = Kpt*error + Kit*integral + Kdt*derivative; // should be -12000 to 12000
+            set_tank(voltage, voltage);
+            pros::delay(10);
+        }
+        set_tank(0, 0);
+    }
+}
