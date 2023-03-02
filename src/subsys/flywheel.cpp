62174d656bea17905c25eeebff6561bd0541c79c
Ryan Richter <ryanrichter028@gmail.com>
2023-03-02 11:57:40 -0500

    adjusted tuning process

--- src/subsys/flywheel.cpp
+++ src/subsys/flywheel.cpp
@@ -59,6 +59,9 @@ namespace flywheel
             voltage = Kv*velocity + Kp*error + Ki*integral + Kd*derivative;
             // cout << "voltage = " << voltage << "\n";
             prev_error = error;
+            // do not brake to get to 0
+            if (!velocity)
+                voltage = 0;
             m::flywheel1.move_voltage(voltage);
             m::flywheel2.move_voltage(voltage);
             pros::delay(10);
@@ -115,49 +118,36 @@ namespace flywheel
             // wait until at close speed
             velocity = vclose;
             tune_wait();
-            // pros::delay(500);
             t1 = pros::millis();
+            pros::delay(200);
             // wait until stopped
             velocity = 0;
             tune_wait();
-            // pros::delay(500);
+            pros::delay(200);
             t2 = pros::millis();
             // wait until at far speed
             velocity = vfar;
             tune_wait();
-            // pros::delay(500);
             t3 = pros::millis();
+            pros::delay(200);
             velocity = 0;
             // wait until stopped
-            // pros::delay(500);
+            pros::delay(200);
             t4 = pros::millis();
 
 
-            delta_t = t4-t0;
+            delta_t = (t3-t2) + (t1-t0); // ignore time to stop
             avg_delta_t += delta_t;
             cout << 
             Kp << "," <<
             delta_t << ", ," <<
             t0 << "," <<
-            t1 << "," <<
-            t2 << "," <<
-            t3 << "," <<
-            t4 << "\n";
+            t1-t0 << "," <<
+            t2-t1 << "," <<
+            t3-t2 << "," <<
+            t4-t3 << "\n";
             }
             cout << Kp << "," << avg_delta_t << endl;
-
-// 0,21160, ,982,10992,12132,22142,22142
-// 1000,3040, ,22142,22472,22862,25182,25182
-// 2000,3050, ,25182,25542,25932,28232,28232
-// 1000,2720, ,28232,28262,28712,30952,30952
-// 1500,4350, ,30952,32582,32972,35302,35302
-// 1000,3340, ,35302,35872,36262,38642,38642
-// 750,3540, ,38642,39312,39692,42182,42182
-// 625,3780, ,42182,43062,43462,45962,45962
-// 563,3680, ,45962,46762,47142,49642,49642
-// 532,3760, ,49642,50422,50902,53402,53402
-// 517,3680, ,53402,54052,54492,57082,57082
-
 
             if (avg_delta_t<min_delta_t)
             {
